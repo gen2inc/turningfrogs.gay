e8698dcee247bdf1d5e3856625ecbbcf587383ee
ellie <81702700+yeeterlol@users.noreply.github.com>
2023-07-25 20:42:08 -0700

    basically fix and improve the minecraft status [+] added an actual working api [+] added copy ip [+] added loading

--- script/main.js
+++ script/main.js
@@ -1,14 +1,22 @@
 import * as utils from "./utils.js"
 
 async function connect() {
+    document.getElementById("mc-status").style.color = "yellow";
+    document.getElementById("mc-status").innerHTML = "Loading....";
     let connection = await utils.attemptConnection();
 
     if (connection === "FAIL") {
         document.getElementById("mc-status").innerHTML = "Error - Check console"
-    } else if (connection.status === "success") {
+    } else if (connection.ip != "127.0.0.1") {
         document.getElementById("mc-status").style.color = "green";
-        document.getElementById("mc-status").innerHTML = `Online - ${connection.players.now}/${connection.players.max} - ${connection.server.name.replace("Paper", "")}`
+        document.getElementById("mc-status").innerHTML = `Online - ${connection.players.online}/${connection.players.max} - ${connection.protocol_name}`
     }
 }
+
+document.getElementById('mc-card').onclick = function(e){
+    utils.copyText("mcis.turningfrogs.gay")
+    alert("Copied IP!")
+}
+
 connect()
 
--- script/utils.js
+++ script/utils.js
@@ -1,17 +1,49 @@
 export async function attemptConnection(){
     try {
-        const request = await fetch('https://mcapi.us/server/status?ip='+"mcis.turningfrogs.gay");
+        const request = await fetch("https://api.mcsrvstat.us/2/mcis.turningfrogs.gay");
         const jason = await request.json()
 
-        // if rate limit or some shit idk
-        if (jason.status) {
-            return jason;
-        } else {
-            return "FAIL";
-        }
+        return jason;
     } catch (e) {
+        console.log(e);
         return "FAIL";
     }
 }
+export function fallbackCopy(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+}
+export function copyText(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      // lol
+    }, function(err) {
+      console.error(err);
+      prompt("oopsie! you caught an error :'(, copy this text to join! :3", "mcis.turningfrogs.gay")
+    });
+}
+
 
 export default attemptConnection;
