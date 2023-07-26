d5ca3f100917d54b406f28999342c4d208723cb5
ellie <81702700+yeeterlol@users.noreply.github.com>
2023-07-25 19:17:24 -0700

    fix shit
    replace paper 1.19.4 with 1.19.4

--- script/main.js
+++ script/main.js
@@ -3,12 +3,11 @@ import * as utils from "./utils.js"
 async function connect() {
     let connection = await utils.attemptConnection();
 
-    console.log(connection);
     if (connection === "FAIL") {
         document.getElementById("mc-status").innerHTML = "Error - Check console"
     } else if (connection.status === "success") {
         document.getElementById("mc-status").style.color = "green";
-        document.getElementById("mc-status").innerHTML = `Online - ${connection.players.now}/${connection.players.max} - ${connection.server.name}`
+        document.getElementById("mc-status").innerHTML = `Online - ${connection.players.now}/${connection.players.max} - ${connection.server.name.replace("Paper", "")}`
     }
 }
 connect()
--- script/utils.js
+++ script/utils.js
@@ -2,8 +2,12 @@ export async function attemptConnection(){
     try {
         const request = await fetch('https://mcapi.us/server/status?ip='+"mcis.turningfrogs.gay");
         const jason = await request.json()
+
+        // if rate limit or some shit idk
         if (jason.status) {
             return jason;
+        } else {
+            return "FAIL";
         }
     } catch (e) {
         return "FAIL";
