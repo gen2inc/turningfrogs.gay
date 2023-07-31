eed1e409145e2c7750d3628009fe9271ff95d8a9
ellie <81702700+yeeterlol@users.noreply.github.com>
2023-07-30 17:57:43 -0700

    websocket server
    add websocket instead of this shit

--- script/main.js
+++ script/main.js
@@ -1,10 +1,13 @@
 import * as utils from "./utils.js"
 
+
+
 async function connect() {
+    
     document.getElementById("mc-status").style.color = "yellow";
     document.getElementById("mc-status").innerHTML = "Loading....";
     let connection = await utils.attemptConnection();
-
+    
     if (connection === "FAIL") {
         document.getElementById("mc-status").style.color = "red";
         document.getElementById("mc-status").innerHTML = "Error - Check console"
@@ -15,6 +18,38 @@ async function connect() {
         document.getElementById("mc-status").style.color = "red";
         document.getElementById("mc-status").innerHTML = "Not Online";
     }
+
+   // we need to host the web socket server
+   /* 
+   function cleanup() {
+       ws.removeEventListener("message", messageHandler);
+       ws.removeEventListener("error", errorHandler);
+       ws.close();
+   }
+
+   function messageHandler(event) {
+       if (event.data === "error") { 
+           document.getElementById("mc-status").style.color = "red";
+           document.getElementById("mc-status").innerHTML = "Offline";
+       } else {
+           const jason = JSON.parse(event.data);
+           document.getElementById("mc-status").style.color = "green";
+           document.getElementById("mc-status").innerHTML = `Online - ${jason.online}/${jason.max} - ${jason.ver}`
+       }
+   }
+
+   function errorHandler(event) {
+       document.getElementById("mc-status").style.color = "red";
+       document.getElementById("mc-status").innerHTML = "Error - Check console"
+   }
+   const ws = new WebSocket("ws://127.0.0.1:8080");
+    ws.addEventListener("message", messageHandler);
+    ws.addEventListener("error",  errorHandler)
+
+    window.addEventListener("beforeunload", () => {
+        cleanup();
+    })
+    */
 }
 
 document.getElementById('mc-card').onclick = function(e){
